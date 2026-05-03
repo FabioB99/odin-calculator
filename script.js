6761d4c804bac2c4522fc424420c89096e7e5150
FabioB99 <105454616+FabioB99@users.noreply.github.com>
2026-05-03 15:26:05 +0200

    Add functions to perform math operations

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 console.log("Hello World");
+
+function add(number1, number2) {
+    return number1 + number2;
+}
+
+function substract(number1, number2) {
+    return number1 - number2;
+}
+
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+function divide(number1, number2) {
+    return number1 / number2;
+}
+
+
+console.log(add(1,3));
+console.log(substract(3,2));
+console.log(multiply(3,2));
+console.log(divide(4,2));
